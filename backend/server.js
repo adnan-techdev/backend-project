0e865d48aa98fd214e308af2ef737aa942678dd7
Adnan Amir <adnanamir.devverx@gmail.com>
2026-09-17 19:20:25 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,24 +13,7 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-  const allowedOrigin = (origin, callback) => {
-  if (
-    !origin ||
-    origin === process.env.FRONTEND_URL ||
-    origin.endsWith(".vercel.app")
-  ) {
-    callback(null, true);
-  } else {
-    callback(new Error("Not allowed by CORS"));
-  }
-};
- 
-app.use(
-  cors({
-    origin: allowedOrigin,
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use(express.json());
 
